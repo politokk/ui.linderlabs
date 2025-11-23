5de2f3629be214dcbdb8a1a4815d949438664f47
politokk <ellielinder2023@gmail.com>
2025-11-22 20:26:24 -0500

    rreeg

--- app/demo/[name]/blocks/sink/(pages)/react-hook-form/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ExampleForm } from "./example-form"
-
-export default function ReactHookFormPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-      <ExampleForm />
-    </div>
-  )
-}
