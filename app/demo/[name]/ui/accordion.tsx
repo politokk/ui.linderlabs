0c179c5e1f1af97454c44c7aa5dce671e7929165
politokk <ellielinder2023@gmail.com>
2025-11-22 19:00:50 -0500

    iconss

--- app/demo/[name]/ui/accordion.tsx
+++ app/demo/[name]/ui/accordion.tsx
@@ -6,39 +6,23 @@ import {
 import { AccordionDemo } from "@/components/demos/accordion/accordion-demo";
 import { AccordionExtensive } from "@/components/demos/accordion/accordion-extensive";
 import { ComponentDisplay } from "@/components/display/component-display"
+import { ComponentWrapper } from "@/components/display/component-wrapper"
 
 export const dynamic = "force-dynamic"
 
-const components = [
-  {
-    name: "accordion-demo",
-    path: "accordion/accordion-demo",
-    icon: <FolderRootIcon />,
-    component: AccordionDemo,
-  },
-  {
-    name: "accordion-extensive",
-    path: "accordion/accordion-extensive",
-    icon: <ScrollTextIcon />,
-    component: AccordionExtensive,
-  },
-]
 
-export default async function AccordionPage() {
+export default function AccordionPage() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 mt-15">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {components.map((comp) => (
-          <ComponentDisplay
-            key={comp.name}
-            path={comp.path}
-            icon={comp.icon}
-            className="w-full max-w-md mx-auto py-0"
-          >
-            <comp.component />
-          </ComponentDisplay>
-        ))}
-      </div>
-    </div>
+
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <ComponentWrapper name="Accordion">
+              <AccordionDemo />
+            </ComponentWrapper>
+            <ComponentWrapper name="Accordion">
+              <AccordionExtensive />
+            </ComponentWrapper>
+          </div>
+          </div>
   )
 }
