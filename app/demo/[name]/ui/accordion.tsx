4bdab3df2363c4565fc3adc7f1f87cd946e9b22b
politokk <ellielinder2023@gmail.com>
2025-11-22 19:06:11 -0500

    iconss

--- app/demo/[name]/ui/accordion.tsx
+++ app/demo/[name]/ui/accordion.tsx
@@ -1,13 +1,10 @@
-import {
-  FolderRootIcon,
-  ScrollTextIcon
-} from "lucide-react" 
+
 
 import { AccordionDemo } from "@/components/demos/accordion/accordion-demo";
 import { AccordionExtensive } from "@/components/demos/accordion/accordion-extensive";
 import { ComponentDisplay } from "@/components/display/component-display"
 import { ComponentWrapper } from "@/components/display/component-wrapper"
-
+import { FolderRootIcon, ScrollTextIcon } from "lucide-react"
 export const dynamic = "force-dynamic"
 
 
@@ -16,12 +13,12 @@ export default function AccordionPage() {
 
         <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <ComponentWrapper name="Accordion">
+            <ComponentDisplay path="accordion/accordion-demo" icon={<FolderRootIcon />}>
               <AccordionDemo />
-            </ComponentWrapper>
-            <ComponentWrapper name="Accordion">
+            </ComponentDisplay>
+            <ComponentDisplay path="accordion/accordion-extensive" icon={<ScrollTextIcon />} >
               <AccordionExtensive />
-            </ComponentWrapper>
+            </ComponentDisplay>
           </div>
           </div>
   )
