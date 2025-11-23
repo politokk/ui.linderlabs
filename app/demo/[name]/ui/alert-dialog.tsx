fec0cfc2fc6a5cde196d56bd9b0e0eb32f1554f9
politokk <ellielinder2023@gmail.com>
2025-11-23 10:10:44 -0500

    aspect-ratio

--- app/demo/[name]/ui/alert-dialog.tsx
+++ app/demo/[name]/ui/alert-dialog.tsx
@@ -22,7 +22,7 @@ export default async function AlertDialogPage() {
 
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <ComponentWrapper name="Accordion" icon="FolderRootIcon">
+        <ComponentWrapper name="Alert Dialog" icon="AlertTriangleIcon">
           <AlertDialogDemo />
         </ComponentWrapper>
       </div>
--- app/demo/[name]/ui/aspect-ratio.tsx
+++ app/demo/[name]/ui/aspect-ratio.tsx
@@ -30,7 +30,7 @@ export default async function AspectRatioPage() {
   
 
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 mt-15">
+    <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {components.map((comp) => (
            <ComponentDisplay
